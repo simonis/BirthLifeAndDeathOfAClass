a828b6571ee706622d1111bc81f15e6f66e66c7b
simonis <volker.simonis@gmail.com>
2017-10-29 21:40:44 +0100

    Added class identity slides with quiz

--- examples/src/simonis/TwoLoaders.java
+++ examples/src/simonis/TwoLoaders.java
@@ -5,12 +5,12 @@ import java.net.URLClassLoader;
 
 public class TwoLoaders {
   public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    TwoLoaders tl1 = new TwoLoaders();
     System.out.println(TwoLoaders.class.getResource(".."));
     ClassLoader cl = new URLClassLoader(new URL[] { TwoLoaders.class.getResource("..") }, null);
-    TwoLoaders tl1 = new TwoLoaders();
-    Object o = cl.loadClass("simonis.TwoLoaders").newInstance();
-    System.out.println(o.getClass().getName().equals(tl1.getClass().getName())); // true / false ?
-    System.out.println(o instanceof TwoLoaders);
-    tl1 = (TwoLoaders)o;
+    Object tl2 = cl.loadClass("simonis.TwoLoaders").newInstance();
+    System.out.println(tl2.getClass().getName().equals(tl1.getClass().getName())); // true / false ?
+    System.out.println(tl2 instanceof simonis.TwoLoaders);
+    tl1 = (TwoLoaders)tl2;
   }
 }
