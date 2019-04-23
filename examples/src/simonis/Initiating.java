965f5991c4cb8eb1dc3c42d8ce57bff15975f377
simonis <volker.simonis@gmail.com>
2019-04-23 19:09:53 +0200

    Added some material for initiating/defining class loader topic

--- examples/src/simonis/Initiating.java
+++ examples/src/simonis/Initiating.java
@@ -9,16 +9,17 @@ import java.util.Arrays;
 public class Initiating {
 
   static class MyClassLoader extends URLClassLoader {
-    public MyClassLoader(String arg0, URL[] arg1, ClassLoader arg2) {
-      super(arg0, arg1, arg2);
+    public MyClassLoader(String name, URL[] urls, ClassLoader parent) {
+      super(name, urls, parent);
     }
     public Class<?> myLoadClass(String name, boolean resolve) throws ClassNotFoundException {
       return loadClass(name, resolve);
     }
     public boolean isInitiating(String name) {
       return findLoadedClass(name) != null;
-    }    
+    }
   }
+
   public static void main(String[] args) throws ClassNotFoundException, IOException, InstantiationException, IllegalAccessException {
     MyClassLoader cl1 = new MyClassLoader("cl1", new URL[] { Initiating.class.getResource("..") }, null);
     MyClassLoader cl2 = new MyClassLoader("cl2", new URL[] { Initiating.class.getResource("..") }, cl1) {
@@ -39,11 +40,11 @@ public class Initiating {
     MyClassLoader cl3 = new MyClassLoader("cl3", new URL[] { Initiating.class.getResource("..") }, cl2);
     MyClassLoader cl4 = new MyClassLoader("cl4", new URL[] { Initiating.class.getResource("..") }, cl3);
 
-    //Class<?> cA = cl4.myLoadClass("simonis.Initiating$A", true);
-    Class<?> cA = Class.forName("simonis.Initiating$A", true, cl4);
+    // Class<?> cA = cl4.myLoadClass("simonis.Initiating$A", true);  // Won't make 'cl4' an "initiating" class loader of "A"
+    Class<?> cA = Class.forName("simonis.Initiating$A", true, cl4);  // 'cl4' will become an "initiating" class loader of "A"
 /*
 
-// Class.forName will ultimatively (through JVM_FindClassFromCaller) call the method below with 'class_loader' == 'cl3'
+// Class.forName will ultimately (through JVM_FindClassFromCaller) call the method below with 'class_loader' == 'cl3'
 
 Klass* SystemDictionary::resolve_instance_class_or_null(Symbol* name,
                                                         Handle class_loader,
@@ -63,12 +64,12 @@ Klass* SystemDictionary::resolve_instance_class_or_null(Symbol* name,
 
         check_constraints(d_index, d_hash, k, class_loader, false, THREAD);
 
-// This actually adds 'class_loader()' == 'cl3' as INITIATING loader of 'k' to 'cl3's dictionary 
+// This actually adds 'class_loader()' == 'cl3' as INITIATING loader of 'k' to 'cl3's dictionary
 
               update_dictionary(d_index, d_hash, p_index, p_hash,
                               k, class_loader, THREAD);
-                              
-// i.e. 
+
+// i.e.
 
 void SystemDictionary::update_dictionary(int d_index, unsigned int d_hash,
                                          int p_index, unsigned int p_hash,
@@ -88,23 +89,31 @@ void SystemDictionary::update_dictionary(int d_index, unsigned int d_hash,
 
 JVMLS (https://docs.oracle.com/javase/specs/jvms/se9/html/jvms-5.html)
 
-5.3. Creation and Loading 
+5.3. Creation and Loading
 
 A class loader L may create C by defining it directly or by delegating to another class loader.
 If L creates C directly, we say that L defines C or, equivalently, that L is the defining loader of C.
 
-When one class loader delegates to another class loader, the loader that initiates the loading is not 
-necessarily the same loader that completes the loading and defines the class. 
-If L creates C, either by defining it directly or by delegation, we say that L initiates loading of C or, 
-equivalently, that L is an initiating loader of C. 
+When one class loader delegates to another class loader, the loader that initiates the loading is not
+necessarily the same loader that completes the loading and defines the class.
+If L creates C, either by defining it directly or by delegation, we say that L initiates loading of C or,
+equivalently, that L is an initiating loader of C.
+
+// Initial problem description: "Java is not type-safe", by Vijay Saraswat, AT&T Research,180 Park Avenue, Florham Park NJ 07932
+
+https://www.cis.upenn.edu/~bcpierce/courses/629/papers/Saraswat-javabug.html
+
  */
-    //cA.newInstance();
+    //java.awt.Frame frame = new java.awt.Frame();
+    //System.out.println("Defining = " + frame.getClass().getClassLoader());
+
+    cA.newInstance(); // Will make 'c2' (the defining loader of "A") an initiating loader of "java.awt.Frame"
     System.out.println("Defining = " + cA.getClassLoader().getName());
     System.out.println("cl1 is initiating loader for A: " + cl1.isInitiating("simonis.Initiating$A"));
     System.out.println("cl2 is initiating loader for A: " + cl2.isInitiating("simonis.Initiating$A"));
     System.out.println("cl3 is initiating loader for A: " + cl3.isInitiating("simonis.Initiating$A"));
     System.out.println("cl4 is initiating loader for A: " + cl4.isInitiating("simonis.Initiating$A"));
-    
+
     Instrumentation inst = InstAgent.inst;
     System.out.println(cl1.getName() + " initiating loder for:");
     Arrays.asList(inst.getInitiatedClasses(cl1)).forEach(System.out::println);
@@ -115,9 +124,13 @@ equivalently, that L is an initiating loader of C.
     System.out.println(cl4.getName() + " initiating loder for:");
     Arrays.asList(inst.getInitiatedClasses(cl4)).forEach(System.out::println);
     System.out.println(Initiating.class.getClassLoader().getName() + " initiating loder for:");
-    Arrays.asList(inst.getInitiatedClasses(Initiating.class.getClassLoader())).stream().filter(c -> c.getName().startsWith("simonis")).forEach(System.out::println);
+    Arrays.asList(inst.getInitiatedClasses(Initiating.class.getClassLoader())).stream().filter(c -> c.getName().startsWith("simonis") || c.getName().startsWith("java.awt.Frame")).forEach(System.out::println);
+    System.out.println(Initiating.class.getClassLoader().getParent().getName() + " initiating loder for:");
+    Arrays.asList(inst.getInitiatedClasses(Initiating.class.getClassLoader().getParent())).stream().filter(c -> c.getName().startsWith("simonis") || c.getName().startsWith("java.awt.Frame")).forEach(System.out::println);
+    System.out.println("Bootstrap" + " initiating loder for:");
+    Arrays.asList(inst.getInitiatedClasses(null)).stream().filter(c -> c.getName().startsWith("simonis") || c.getName().startsWith("java.awt.Frame")).forEach(System.out::println);
   }
-  
+
   public static class A {
     static { java.awt.Frame frame = new java.awt.Frame(); }
   }
